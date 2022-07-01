31806c34715b10baa7c9a5bb42e4b02fb5c0de5d
bornittah <bornittah@gmail.com>
2022-07-01 13:42:12 +0300

    add unit tests

--- src/modules/app.test.js
+++ src/modules/app.test.js
@@ -1,3 +1,11 @@
+import { leagues, countLeagues } from './leagues.js';
+import {
+  appId, addComments, fetchComments, countComments, addLikes, fetchLikes
+} from './user-interactions.js';
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.doMock();
+
 describe('Tests', () => {
   document.body.innerHTML = `<header class="flex">
   <a href="#" class="company-logo">LEAGUES</a>
@@ -20,11 +28,39 @@ describe('Tests', () => {
   </p>
 </footer>`;
 
-  test('Number of Comments', () => {
-    const ul = document.querySelectorAll('.comments');
-    ul.innerHTML = '<li>2022/06/29 Aga: good game</li>';
-    const counter = document.querySelectorAll('.comments_count');
-    const comments = ul.childElementCount;
-    expect(counter.textContext).toBe(comments);
-  });
+test('Number of Leagues', async () => {
+  const data = await leagues();
+  const number = await countLeagues();
+  expect(data.length).toBe(number);
+});
+
+test('Number of Comments', async () => {
+  await appId();
+  const key = localStorage.getItem('football');
+  const data = await leagues();
+  const id = data[0].id;
+  const comment = {
+    item_id: id,
+    username: 'Agasha',
+    comment: 'Nice game!',
+  };
+  await addComments(key, comment);
+  const comments = await fetchComments(key, id);
+  const commentCounter = await countComments(id);
+  expect(commentCounter).toBe(comments.length);
+}, 30000);
+
+test('Number of Likes', async () => {
+  await appId();
+  const key = localStorage.getItem('football');
+  const data = await leagues();
+  const id = data[0].id;
+  const like = {
+    item_id: id,
+  };
+  await addLikes(key, like);
+  const likes = await fetchLikes(key, id);
+  expect(likes.length).toBe(1);
+}, 30000);
+
 });
